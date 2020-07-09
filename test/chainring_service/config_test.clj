d22af771f00b6445a28b696d0b051ad2b915ad2e
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-09 10:56:58 +0200

    Added new functions to test into config_test.clj

--- test/chainring_service/config_test.clj
+++ test/chainring_service/config_test.clj
@@ -29,61 +29,67 @@
 
 
 (deftest test-full-prefix-existence
-    "Check that the chainring-service.config/full-prefix definition exists."
-    (testing "if the chainring-service.config/full-prefix definition exists."
-        (is (callable? 'chainring-service.config/full-prefix))))
+  "Check that the chainring-service.config/full-prefix definition exists."
+  (testing "if the chainring-service.config/full-prefix definition exists."
+    (is (callable? 'chainring-service.config/full-prefix))))
 
 
 (deftest test-update-configuration-existence
-    "Check that the chainring-service.config/update-configuration definition exists."
-    (testing "if the chainring-service.config/update-configuration definition exists."
-        (is (callable? 'chainring-service.config/update-configuration))))
+  "Check that the chainring-service.config/update-configuration definition exists."
+  (testing "if the chainring-service.config/update-configuration definition exists."
+    (is (callable? 'chainring-service.config/update-configuration))))
 
 
 (deftest test-load-configuration-from-ini-existence
-    "Check that the chainring-service.config/load-configuration-from-ini definition exists."
-    (testing "if the chainring-service.config/load-configuration-from-ini definition exists."
-        (is (callable? 'chainring-service.config/load-configuration-from-ini))))
+  "Check that the chainring-service.config/load-configuration-from-ini definition exists."
+  (testing "if the chainring-service.config/load-configuration-from-ini definition exists."
+    (is (callable? 'chainring-service.config/load-configuration-from-ini))))
 
 
 (deftest test-pretty-print?-existence
-    "Check that the chainring-service.config/pretty-print? definition exists."
-    (testing "if the chainring-service.config/pretty-print? definition exists."
-        (is (callable? 'chainring-service.config/pretty-print?))))
+  "Check that the chainring-service.config/pretty-print? definition exists."
+  (testing "if the chainring-service.config/pretty-print? definition exists."
+    (is (callable? 'chainring-service.config/pretty-print?))))
 
 
 (deftest test-get-api-prefix-existence
-    "Check that the chainring-service.config/get-api-prefix definition exists."
-    (testing "if the chainring-service.config/get-api-prefix definition exists."
-        (is (callable? 'chainring-service.config/get-api-prefix))))
+  "Check that the chainring-service.config/get-api-prefix definition exists."
+  (testing "if the chainring-service.config/get-api-prefix definition exists."
+    (is (callable? 'chainring-service.config/get-api-prefix))))
 
 
 (deftest test-get-api-version-existence
-    "Check that the chainring-service.config/get-api-version definition exists."
-    (testing "if the chainring-service.config/get-api-version definition exists."
-        (is (callable? 'chainring-service.config/get-api-version))))
+  "Check that the chainring-service.config/get-api-version definition exists."
+  (testing "if the chainring-service.config/get-api-version definition exists."
+    (is (callable? 'chainring-service.config/get-api-version))))
 
 
 (deftest test-get-version-existence
-    "Check that the chainring-service.config/get-version definition exists."
-    (testing "if the chainring-service.config/get-version definition exists."
-        (is (callable? 'chainring-service.config/get-version))))
+  "Check that the chainring-service.config/get-version definition exists."
+  (testing "if the chainring-service.config/get-version definition exists."
+    (is (callable? 'chainring-service.config/get-version))))
 
 
 (deftest test-get-db-version-existence
-    "Check that the chainring-service.config/get-db-version definition exists."
-    (testing "if the chainring-service.config/get-db-version definition exists."
-        (is (callable? 'chainring-service.config/get-db-version))))
+  "Check that the chainring-service.config/get-db-version definition exists."
+  (testing "if the chainring-service.config/get-db-version definition exists."
+    (is (callable? 'chainring-service.config/get-db-version))))
 
 
 (deftest test-get-api-full-prefix-existence
-    "Check that the chainring-service.config/get-api-full-prefix definition exists."
-    (testing "if the chainring-service.config/get-api-full-prefix definition exists."
-        (is (callable? 'chainring-service.config/get-api-full-prefix))))
+  "Check that the chainring-service.config/get-api-full-prefix definition exists."
+  (testing "if the chainring-service.config/get-api-full-prefix definition exists."
+    (is (callable? 'chainring-service.config/get-api-full-prefix))))
 
 
 (deftest test-verbose?-existence
-    "Check that the chainring-service.config/verbose? definition exists."
-    (testing "if the chainring-service.config/verbose? definition exists."
-        (is (callable? 'chainring-service.config/verbose?))))
+  "Check that the chainring-service.config/verbose? definition exists."
+  (testing "if the chainring-service.config/verbose? definition exists."
+    (is (callable? 'chainring-service.config/verbose?))))
+
+
+(deftest test-sap-response?-existence
+  "Check that the chainring-service.config/mock-sap-response? definition exists."
+  (testing "if the chainring-service.config/mock-sap-response? definition exists."
+    (is (callable? 'chainring-service.config/mock-sap-response?))))
 
