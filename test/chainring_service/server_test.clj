918ed73e2ba2e4538b8c5f42f69d4dea36c83184
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-10 09:19:10 +0200

    Reformatted server_test.clj by zprint

--- test/chainring_service/server_test.clj
+++ test/chainring_service/server_test.clj
@@ -19,9 +19,9 @@
 ;
 
 (defn callable?
-    "Test if given function-name is bound to the real function."
-    [function-name]
-    (clojure.test/function? function-name))
+  "Test if given function-name is bound to the real function."
+  [function-name]
+  (clojure.test/function? function-name))
 
 ;
 ; Tests for functions existence
@@ -30,59 +30,64 @@
 (deftest test-floor-id->str-existence
   "Check that the chainring-service.server/floor-id->str definition exists."
   (testing "if the chainring-service.server/floor-id->str definition exists."
-    (is (callable? 'chainring-service.server/floor-id->str))))
+           (is (callable? 'chainring-service.server/floor-id->str))))
 
 
 (deftest test-all-drawings-for-floor-existence
   "Check that the chainring-service.server/all-drawings-for-floor definition exists."
-  (testing "if the chainring-service.server/all-drawings-for-floor definition exists."
+  (testing
+    "if the chainring-service.server/all-drawings-for-floor definition exists."
     (is (callable? 'chainring-service.server/all-drawings-for-floor))))
 
 
 (deftest test-entity-count-existence
   "Check that the chainring-service.server/entity-count definition exists."
   (testing "if the chainring-service.server/entity-count definition exists."
-    (is (callable? 'chainring-service.server/entity-count))))
+           (is (callable? 'chainring-service.server/entity-count))))
 
 
 (deftest test-prepare-drawing-info-existence
   "Check that the chainring-service.server/prepare-drawing-info definition exists."
-  (testing "if the chainring-service.server/prepare-drawing-info definition exists."
+  (testing
+    "if the chainring-service.server/prepare-drawing-info definition exists."
     (is (callable? 'chainring-service.server/prepare-drawing-info))))
 
 
 (deftest test-select-nearest-date-existence
   "Check that the chainring-service.server/select-nearest-date definition exists."
-  (testing "if the chainring-service.server/select-nearest-date definition exists."
+  (testing
+    "if the chainring-service.server/select-nearest-date definition exists."
     (is (callable? 'chainring-service.server/select-nearest-date))))
 
 
 (deftest test-log-process-drawing-info-existence
   "Check that the chainring-service.server/log-process-drawing-info definition exists."
-  (testing "if the chainring-service.server/log-process-drawing-info definition exists."
+  (testing
+    "if the chainring-service.server/log-process-drawing-info definition exists."
     (is (callable? 'chainring-service.server/log-process-drawing-info))))
 
 
 (deftest test-get-api-part-from-uri-existence
   "Check that the chainring-service.server/get-api-part-from-uri definition exists."
-  (testing "if the chainring-service.server/get-api-part-from-uri definition exists."
+  (testing
+    "if the chainring-service.server/get-api-part-from-uri definition exists."
     (is (callable? 'chainring-service.server/get-api-part-from-uri))))
 
 
 (deftest test-get-api-command-existence
   "Check that the chainring-service.server/get-api-command definition exists."
   (testing "if the chainring-service.server/get-api-command definition exists."
-    (is (callable? 'chainring-service.server/get-api-command))))
+           (is (callable? 'chainring-service.server/get-api-command))))
 
 
 (deftest test-api-call-handler-existence
   "Check that the chainring-service.server/api-call-handler definition exists."
   (testing "if the chainring-service.server/api-call-handler definition exists."
-    (is (callable? 'chainring-service.server/api-call-handler))))
+           (is (callable? 'chainring-service.server/api-call-handler))))
 
 
 (deftest test-handler-existence
   "Check that the chainring-service.server/handler definition exists."
   (testing "if the chainring-service.server/handler definition exists."
-    (is (callable? 'chainring-service.server/handler))))
+           (is (callable? 'chainring-service.server/handler))))
 
