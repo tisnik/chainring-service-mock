94a53ba14574552cc63a05907b60fd07f329dd1d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-10 08:56:10 +0200

    Added basic unit tests into rest_api_impl_test.clj

--- test/chainring_service/rest_api_impl_test.clj
+++ test/chainring_service/rest_api_impl_test.clj
@@ -27,3 +27,50 @@
 ; Tests for functions existence
 ;
 
+(deftest test-all-aoids-existence
+  "Check that the chainring-service.rest-api-impl/all-aoids definition exists."
+  (testing "if the chainring-service.rest-api-impl/all-aoids definition exists."
+    (is (callable? 'chainring-service.rest-api-impl/all-aoids))))
+
+
+(deftest test-all-objects-existence
+  "Check that the chainring-service.rest-api-impl/all-objects definition exists."
+  (testing "if the chainring-service.rest-api-impl/all-objects definition exists."
+    (is (callable? 'chainring-service.rest-api-impl/all-objects))))
+
+
+(deftest test-buildings-existence
+  "Check that the chainring-service.rest-api-impl/buildings definition exists."
+  (testing "if the chainring-service.rest-api-impl/buildings definition exists."
+    (is (callable? 'chainring-service.rest-api-impl/buildings))))
+
+
+(deftest test-floors-existence
+  "Check that the chainring-service.rest-api-impl/floors definition exists."
+  (testing "if the chainring-service.rest-api-impl/floors definition exists."
+    (is (callable? 'chainring-service.rest-api-impl/floors))))
+
+
+(deftest test-rooms-existence
+  "Check that the chainring-service.rest-api-impl/rooms definition exists."
+  (testing "if the chainring-service.rest-api-impl/rooms definition exists."
+    (is (callable? 'chainring-service.rest-api-impl/rooms))))
+
+
+(deftest test-building-existence
+  "Check that the chainring-service.rest-api-impl/building definition exists."
+  (testing "if the chainring-service.rest-api-impl/building definition exists."
+    (is (callable? 'chainring-service.rest-api-impl/building))))
+
+
+(deftest test-floor-existence
+  "Check that the chainring-service.rest-api-impl/floor definition exists."
+  (testing "if the chainring-service.rest-api-impl/floor definition exists."
+    (is (callable? 'chainring-service.rest-api-impl/floor))))
+
+
+(deftest test-room-existence
+  "Check that the chainring-service.rest-api-impl/room definition exists."
+  (testing "if the chainring-service.rest-api-impl/room definition exists."
+    (is (callable? 'chainring-service.rest-api-impl/room))))
+
