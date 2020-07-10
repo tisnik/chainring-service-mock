01b7a57a0a4b0a25d7f48d2e482bb7678a3e013f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-10 08:56:10 +0200

    Added basic unit tests into rest_api_utils_test.clj

--- test/chainring_service/rest_api_utils_test.clj
+++ test/chainring_service/rest_api_utils_test.clj
@@ -27,3 +27,68 @@
 ; Tests for functions existence
 ;
 
+(deftest test-read-request-body-existence
+  "Check that the chainring-service.rest-api-utils/read-request-body definition exists."
+  (testing "if the chainring-service.rest-api-utils/read-request-body definition exists."
+    (is (callable? 'chainring-service.rest-api-utils/read-request-body))))
+
+
+(deftest test-body->results-existence
+  "Check that the chainring-service.rest-api-utils/body->results definition exists."
+  (testing "if the chainring-service.rest-api-utils/body->results definition exists."
+    (is (callable? 'chainring-service.rest-api-utils/body->results))))
+
+
+(deftest test-send-response-existence
+  "Check that the chainring-service.rest-api-utils/send-response definition exists."
+  (testing "if the chainring-service.rest-api-utils/send-response definition exists."
+    (is (callable? 'chainring-service.rest-api-utils/send-response))))
+
+
+(deftest test-send-response-with-cookie-existence
+  "Check that the chainring-service.rest-api-utils/send-response-with-cookie definition exists."
+  (testing "if the chainring-service.rest-api-utils/send-response-with-cookie definition exists."
+    (is (callable? 'chainring-service.rest-api-utils/send-response-with-cookie))))
+
+
+(deftest test-send-ok-response-existence
+  "Check that the chainring-service.rest-api-utils/send-ok-response definition exists."
+  (testing "if the chainring-service.rest-api-utils/send-ok-response definition exists."
+    (is (callable? 'chainring-service.rest-api-utils/send-ok-response))))
+
+
+(deftest test-send-error-response-existence
+  "Check that the chainring-service.rest-api-utils/send-error-response definition exists."
+  (testing "if the chainring-service.rest-api-utils/send-error-response definition exists."
+    (is (callable? 'chainring-service.rest-api-utils/send-error-response))))
+
+
+(deftest test-send-error-response-wrong-date-existence
+  "Check that the chainring-service.rest-api-utils/send-error-response-wrong-date definition exists."
+  (testing "if the chainring-service.rest-api-utils/send-error-response-wrong-date definition exists."
+    (is (callable? 'chainring-service.rest-api-utils/send-error-response-wrong-date))))
+
+
+(deftest test-send-plain-response-existence
+  "Check that the chainring-service.rest-api-utils/send-plain-response definition exists."
+  (testing "if the chainring-service.rest-api-utils/send-plain-response definition exists."
+    (is (callable? 'chainring-service.rest-api-utils/send-plain-response))))
+
+
+(deftest test-get-timestamp-existence
+  "Check that the chainring-service.rest-api-utils/get-timestamp definition exists."
+  (testing "if the chainring-service.rest-api-utils/get-timestamp definition exists."
+    (is (callable? 'chainring-service.rest-api-utils/get-timestamp))))
+
+
+(deftest test-current-time-existence
+  "Check that the chainring-service.rest-api-utils/current-time definition exists."
+  (testing "if the chainring-service.rest-api-utils/current-time definition exists."
+    (is (callable? 'chainring-service.rest-api-utils/current-time))))
+
+
+(deftest test-valid-date?-existence
+  "Check that the chainring-service.rest-api-utils/valid-date? definition exists."
+  (testing "if the chainring-service.rest-api-utils/valid-date? definition exists."
+    (is (callable? 'chainring-service.rest-api-utils/valid-date?))))
+
