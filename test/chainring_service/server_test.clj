6d5fd1c8b38b5164e3204fdae8bd6761b059dfc3
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-10 08:56:10 +0200

    Added basic unit tests into server_test.clj

--- test/chainring_service/server_test.clj
+++ test/chainring_service/server_test.clj
@@ -27,3 +27,62 @@
 ; Tests for functions existence
 ;
 
+(deftest test-floor-id->str-existence
+  "Check that the chainring-service.server/floor-id->str definition exists."
+  (testing "if the chainring-service.server/floor-id->str definition exists."
+    (is (callable? 'chainring-service.server/floor-id->str))))
+
+
+(deftest test-all-drawings-for-floor-existence
+  "Check that the chainring-service.server/all-drawings-for-floor definition exists."
+  (testing "if the chainring-service.server/all-drawings-for-floor definition exists."
+    (is (callable? 'chainring-service.server/all-drawings-for-floor))))
+
+
+(deftest test-entity-count-existence
+  "Check that the chainring-service.server/entity-count definition exists."
+  (testing "if the chainring-service.server/entity-count definition exists."
+    (is (callable? 'chainring-service.server/entity-count))))
+
+
+(deftest test-prepare-drawing-info-existence
+  "Check that the chainring-service.server/prepare-drawing-info definition exists."
+  (testing "if the chainring-service.server/prepare-drawing-info definition exists."
+    (is (callable? 'chainring-service.server/prepare-drawing-info))))
+
+
+(deftest test-select-nearest-date-existence
+  "Check that the chainring-service.server/select-nearest-date definition exists."
+  (testing "if the chainring-service.server/select-nearest-date definition exists."
+    (is (callable? 'chainring-service.server/select-nearest-date))))
+
+
+(deftest test-log-process-drawing-info-existence
+  "Check that the chainring-service.server/log-process-drawing-info definition exists."
+  (testing "if the chainring-service.server/log-process-drawing-info definition exists."
+    (is (callable? 'chainring-service.server/log-process-drawing-info))))
+
+
+(deftest test-get-api-part-from-uri-existence
+  "Check that the chainring-service.server/get-api-part-from-uri definition exists."
+  (testing "if the chainring-service.server/get-api-part-from-uri definition exists."
+    (is (callable? 'chainring-service.server/get-api-part-from-uri))))
+
+
+(deftest test-get-api-command-existence
+  "Check that the chainring-service.server/get-api-command definition exists."
+  (testing "if the chainring-service.server/get-api-command definition exists."
+    (is (callable? 'chainring-service.server/get-api-command))))
+
+
+(deftest test-api-call-handler-existence
+  "Check that the chainring-service.server/api-call-handler definition exists."
+  (testing "if the chainring-service.server/api-call-handler definition exists."
+    (is (callable? 'chainring-service.server/api-call-handler))))
+
+
+(deftest test-handler-existence
+  "Check that the chainring-service.server/handler definition exists."
+  (testing "if the chainring-service.server/handler definition exists."
+    (is (callable? 'chainring-service.server/handler))))
+
